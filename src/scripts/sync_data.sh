f6ba9e42fb55423b7233f5c4f24341c0bb3af5f3
llouice <g13920228592@gmail.com>
2019-05-12 23:42:54 +0800

    添加 sync_data.sh

---
+++ src/scripts/sync_data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# 服务器间同步数据集
+server_75="llouice@10.108.208.113"
+server_lzk="lzk@10.112.101.47"
+server_68="llouice@10.108.209.96"
+
+Sohu2019="/home/llouice/Projects/BERT/Sohu2019"
+target_dir="."
+
+function init() {
+    scp -r "${server_75}:${Sohu2019}" ${target_dir}
+    echo "copy Sohu2019 over!"
+}
+
+init
+
+
+
