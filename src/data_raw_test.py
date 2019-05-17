199926f52d955e3546f1af59377b4bfa0ac9f283
llouice <g13920228592@gmail.com>
2019-05-17 19:06:51 +0800

    修复空 content bug

--- src/data_raw_test.py
+++ src/data_raw_test.py
@@ -3,10 +3,10 @@ from sklearn.model_selection import train_test_split
 
 '''
 将新闻和标记写入txt文件
-每一个句子空一行 没一则新闻空两行
+每一个句子空一行 每一则新闻空两行
 '''
 file = "../datasets/news_test.pkl"
-
+split = "627de65d"
 data = load_data(file)
 print("all data: ", len(data))
 
@@ -28,18 +28,26 @@ def data2txt(data):
         title = news["title"]
         contents = news["content"]
         for t in title:
-            line = t
-            f.write(line)
-            f.write("\n")
-        f.write("\n")
-        for content in contents:
-            for t in content:
-                line = t
+            line = t.strip()
+            line = line.replace("\n", "")
+            line = line.replace("\r", "")
+            if len(line) > 0:
                 f.write(line)
                 f.write("\n")
-            f.write("\n")
-        # 下一新闻
         f.write("\n")
+        for content in contents:
+            # 避免空content
+            if len(content)>0:
+                for t in content:
+                    line = t.strip()
+                    line = line.replace("\n", "")
+                    line = line.replace("\r", "")
+                    if len(line)>0:
+                        f.write(line)
+                        f.write("\n")
+                f.write("\n")
+        # 下一新闻
+        f.write(split + "\n")
         count += 1
         # if count > 2:
         #     break
